d6ca679bc383b6e3432a184f3a12002fcc4e0e5b
TitouanUniLu <titouan.guerin.001@student.uni.lu>
2021-12-05 11:11:01 +0100

    Created Kaleidos.py

---
+++ Proj3_PythonConvertion/Kaleidos.py
@@ -0,0 +1,13 @@
+class Kaleidos:
+    accessory = []
+    structVert = []
+
+    def __init__(self, cycle, nb_notes):
+        self.cycle = cycle
+        self.nb_notes = nb_notes
+
+    @staticmethod
+    def composanteCalc(cycle):
+        composante = []
+
+        return True
